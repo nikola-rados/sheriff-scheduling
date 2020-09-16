c402f8dd6bc7c539389e34ca89096623410b9b08
Travis Semple <travis8814@gmail.com>
2020-09-16 09:48:57 -0700

    Rename more folders.

--- api/controllers/AuthController.cs
+++ api/controllers/AuthController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
