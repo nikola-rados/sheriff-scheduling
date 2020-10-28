fe5a052965c60e2897b2845e4667826cc3144b41
Travis Semple <travis8814@gmail.com>
2020-10-28 16:05:23 -0700

    DropTable

--- db/migrations/20201027213900_ShiftDutyAssignment.cs
+++ db/migrations/20201027213900_ShiftDutyAssignment.cs
@@ -8,6 +8,9 @@ namespace SS.Db.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "ShiftSheriff");
+
             migrationBuilder.DropColumn(
                 name: "Slots",
                 table: "Shift");
