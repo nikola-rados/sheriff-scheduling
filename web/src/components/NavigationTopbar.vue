cfef8a655e0bf1fbb52c1cf7cfbb8b2dde117798
Marzieh Mehrnejad <marzmehr@uvic.ca>
2020-12-10 19:21:01 -0800

    SS-287: Add functionality to disable the shift Schedule drop-down if user doesn't have permission to access the "manage schedule" and "distribute schedule", Add functionality in the shift Schedule drop-down to allow navigation to shift pages based on permissions

--- web/src/components/NavigationTopbar.vue
+++ web/src/components/NavigationTopbar.vue
@@ -19,119 +19,53 @@
 			</b-navbar-brand>
 			<b-navbar-nav class="mt-1 mx-5">
 				<b-nav-item to="/duty-roster" ><div style="display: inline-block; white-space: nowrap;">Duty Roster</div></b-nav-item>         
-					<b-nav-item-dropdown text="Shift Schedule" dropdown >
-						<b-dropdown-item to="/manage-shift-schedule">Manage Schedule</b-dropdown-item>
-						<b-dropdown-item to="/distribute-shift-schedule">Distribute Schedule</b-dropdown-item>
-					</b-nav-item-dropdown>
-					<b-nav-item-dropdown text="My Team" dropdown >
-						<b-dropdown-item to="/team-members">My Team Members</b-dropdown-item>
-						<b-dropdown-item to="/define-roles-access">Define Roles & Access</b-dropdown-item>
-					</b-nav-item-dropdown>
-					<b-nav-item-dropdown text="Manage Types" dropdown :disabled="!hasPermissionToEditManageTypes">
-						<b-dropdown-item to="/assignment-types">Assignment Types</b-dropdown-item>
-						<b-dropdown-item to="/leave-training-types">Leave & Training Types</b-dropdown-item>
-					</b-nav-item-dropdown>
+                <b-nav-item-dropdown text="Shift Schedule" dropdown :disabled="!hasPermissionToViewSchedulePages">
+                    <b-dropdown-item v-if="hasPermissionToViewManageSchedule" to="/manage-shift-schedule">Manage Schedule</b-dropdown-item>
+                    <b-dropdown-item v-if="hasPermissionToViewDistributeSchedule" to="/distribute-shift-schedule">Distribute Schedule</b-dropdown-item>
+                </b-nav-item-dropdown>
+                <b-nav-item-dropdown text="My Team" dropdown >
+                    <b-dropdown-item to="/team-members">My Team Members</b-dropdown-item>
+                    <b-dropdown-item to="/define-roles-access">Define Roles & Access</b-dropdown-item>
+                </b-nav-item-dropdown>
+                <b-nav-item-dropdown text="Manage Types" dropdown :disabled="!hasPermissionToEditManageTypes">
+                    <b-dropdown-item to="/assignment-types">Assignment Types</b-dropdown-item>
+                    <b-dropdown-item to="/leave-training-types">Leave & Training Types</b-dropdown-item>
+                </b-nav-item-dropdown>
 			</b-navbar-nav>
 			<b-navbar-nav class="ml-5 mt-1 mr-5">
-					<b-input-group v-if="locationDataReady" class="mr-2 mt-1" style="height: 40px">
-						<b-input-group-prepend is-text>
-							<b-icon icon="globe"></b-icon>
-						</b-input-group-prepend>
-						<b-form-select
-							style="height: 100%;"
-							v-model="selectedLocation"                
-							:disabled="disableLocationChange"
-							@change="UpdateLocation(selectedLocation)"                
-							>
-							<b-form-select-option
-							v-for="location in locationList"
-							:key="location.id"                  
-							:value="location">{{location.name}}
-							</b-form-select-option>                  
-						</b-form-select>
-					</b-input-group>
-					<b-nav-item-dropdown class="mb-3" dropdown>
-						<template v-slot:button-content>
-							<b-icon-person-circle></b-icon-person-circle>
-						</template>
-                        <b-dropdown-text>USER</b-dropdown-text>
-						<b-dropdown-item-button variant="danger" @click="signout()">Sign out</b-dropdown-item-button>
-					</b-nav-item-dropdown>
-					<b-nav-item-dropdown class="mb-3 mr-5" dropdown>
-						<template v-slot:button-content>
-							<b-icon-gear-fill></b-icon-gear-fill>
-						</template>                        
-						<b-dropdown-item-button>PlaceHolder</b-dropdown-item-button>
-					</b-nav-item-dropdown>
+                <b-input-group v-if="locationDataReady" class="mr-2 mt-1" style="height: 40px">
+                    <b-input-group-prepend is-text>
+                        <b-icon icon="globe"></b-icon>
+                    </b-input-group-prepend>
+                    <b-form-select
+                        style="height: 100%;"
+                        v-model="selectedLocation"                
+                        :disabled="disableLocationChange"
+                        @change="UpdateLocation(selectedLocation)"                
+                        >
+                        <b-form-select-option
+                        v-for="location in locationList"
+                        :key="location.id"                  
+                        :value="location">{{location.name}}
+                        </b-form-select-option>                  
+                    </b-form-select>
+                </b-input-group>
+                <b-nav-item-dropdown class="mb-3" dropdown>
+                    <template v-slot:button-content>
+                        <b-icon-person-circle></b-icon-person-circle>
+                    </template>
+                    <b-dropdown-text>USER</b-dropdown-text>
+                    <b-dropdown-item-button variant="danger" @click="signout()">Sign out</b-dropdown-item-button>
+                </b-nav-item-dropdown>
+                <b-nav-item-dropdown class="mb-3 mr-5" dropdown>
+                    <template v-slot:button-content>
+                        <b-icon-gear-fill></b-icon-gear-fill>
+                    </template>                        
+                    <b-dropdown-item-button>PlaceHolder</b-dropdown-item-button>
+                </b-nav-item-dropdown>
 			</b-navbar-nav>
 		</b-navbar>    
 	</header>
-  <!-- <header class="app-header">
-    <b-navbar toggleable="lg" class="navbar navbar-expand-lg navbar-dark">    
-      <b-navbar-brand class="mt-1" href="https://www2.gov.bc.ca">
-          <img 
-              class="img-fluid d-none d-lg-block"          
-              src="../../public/images/bcid-logo-rev-en.svg"
-              width="177"
-              height="44"
-              alt="B.C. Government Logo"
-            />
-          <img
-            class="img-fluid d-lg-none"
-            src="../../public/images/bcid-symbol-rev.svg"
-            width="63"
-            height="44"
-            alt="B.C. Government Logo"
-          />
-      </b-navbar-brand>
-      <b-navbar-nav class="mt-1 mx-5">
-        <b-nav-item to="/duty-roster" ><div style="display: inline-block; white-space: nowrap;">Duty Roster</div></b-nav-item>         
-          <b-nav-item-dropdown text="Shift Schedule" dropdown :disabled="!hasPermissionToViewSchedulePages">
-            <b-dropdown-item v-if="hasPermissionToViewManageSchedule" to="/manage-shift-schedule">Manage Schedule</b-dropdown-item>
-            <b-dropdown-item v-if="hasPermissionToViewDistributeSchedule" to="/distribute-shift-schedule">Distribute Schedule</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item-dropdown text="My Team" dropdown >
-            <b-dropdown-item to="/team-members">My Team Members</b-dropdown-item>
-            <b-dropdown-item to="/define-roles-access">Define Roles & Access</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item-dropdown text="Manage Types" dropdown :disabled="!hasPermissionToEditManageTypes">
-            <b-dropdown-item to="/assignment-types">Assignment Types</b-dropdown-item>
-            <b-dropdown-item to="/leave-training-types">Leave & Training Types</b-dropdown-item>
-          </b-nav-item-dropdown>
-      </b-navbar-nav>
-      <b-navbar-nav class="ml-5 mt-1 mr-5">
-          <b-input-group v-if="locationDataReady" class="mr-2 mt-1" style="height: 40px">
-            <b-input-group-prepend is-text>
-              <b-icon icon="globe"></b-icon>
-            </b-input-group-prepend>
-            <b-form-select
-              style="height: 100%;"
-              v-model="selectedLocation"                
-              :disabled="disableLocationChange"
-              @change="UpdateLocation(selectedLocation)"                
-              >
-              <b-form-select-option
-              v-for="location in locationList"
-              :key="location.id"                  
-              :value="location">{{location.name}}
-              </b-form-select-option>                  
-            </b-form-select>
-          </b-input-group>
-          <b-nav-item-dropdown class="mb-3" dropdown>
-            <template v-slot:button-content>
-              <b-icon-person-circle></b-icon-person-circle>
-            </template>
-            <b-dropdown-item-button>PlaceHolder</b-dropdown-item-button>
-          </b-nav-item-dropdown>
-          <b-nav-item-dropdown class="mb-3 mr-5" dropdown>
-            <template v-slot:button-content>
-              <b-icon-gear-fill></b-icon-gear-fill>
-            </template>
-            <b-dropdown-item-button>PlaceHolder</b-dropdown-item-button>
-          </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-navbar>    
-  </header> -->
 </template>
 
 <script lang="ts">
@@ -172,20 +106,18 @@
         hasPermissionToViewDistributeSchedule = false;
         hasPermissionToViewManageSchedule = false;
         hasPermissionToViewSchedulePages = false;
-        // manageSchedulePagePermissions = ["ViewMyShifts", "ViewAllShiftsAtMyLocation", "ViewAllShifts"]
         
         mounted() {
-        this.getModulePermissions();
-        this.getCurrentLocation();
-        this.disableLocationChange = false;
+            this.getModulePermissions();
+            this.getCurrentLocation();
+            this.disableLocationChange = false;
         }
 
         public getModulePermissions() {
-        this.hasPermissionToEditManageTypes = this.userDetails.permissions.includes("EditTypes");
-        this.hasPermissionToViewDistributeSchedule = this.userDetails.permissions.includes("ViewDistributeSchedule");
-        // this.hasPermissionToViewManageSchedule = this.manageSchedulePagePermissions.some(permission=> this.userDetails.permissions.includes(permission))
-        console.log(this.hasPermissionToViewManageSchedule)
-        this.hasPermissionToViewSchedulePages = this.hasPermissionToViewDistributeSchedule || this.hasPermissionToViewManageSchedule;
+            this.hasPermissionToEditManageTypes = this.userDetails.permissions.includes("EditTypes");
+            this.hasPermissionToViewDistributeSchedule = this.userDetails.permissions.includes("ViewDistributeSchedule");
+            this.hasPermissionToViewManageSchedule = this.userDetails.permissions.includes("ViewShifts");
+            this.hasPermissionToViewSchedulePages = this.hasPermissionToViewDistributeSchedule || this.hasPermissionToViewManageSchedule;
         }
 
 		public getCurrentLocation()
