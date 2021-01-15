680a8990d5eae3dd382f3c369a5ae06e6140e996
Travis Semple <travis8814@gmail.com>
2021-01-15 10:19:10 -0800

    Small tweak to permissions.

--- api/controllers/scheduling/DistributeScheduleController.cs
+++ api/controllers/scheduling/DistributeScheduleController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Mapster;
 using Microsoft.AspNetCore.Mvc;
@@ -42,21 +43,36 @@ namespace SS.Api.controllers.scheduling
             if (!PermissionDataFiltersExtensions.HasAccessToLocation(User, Db, locationId)) return Forbid();
             if (!User.HasPermission(Permission.ViewDutyRoster)) includeWorkSection = false;
 
-            if (!User.HasPermission(Permission.ViewAllFutureShifts))
+            var shiftAvailability = await ShiftService.GetShiftAvailability(start, end, locationId: locationId);
+            var shiftsWithDuties = await DistributeScheduleService.GetDistributeSchedule(shiftAvailability, includeWorkSection, start, end, locationId);
+
+            if (!User.HasPermission(Permission.ViewAllFutureShifts) ||
+                !User.HasPermission(Permission.ViewDutyRosterInFuture))
             {
                 var location = await Db.Location.AsNoTracking().FirstOrDefaultAsync(l => l.Id == locationId);
                 var timezone = location.Timezone;
-                var restrictionDays = int.Parse(Configuration.GetNonEmptyValue("ViewShiftRestrictionDays"));
                 var currentDate = DateTimeOffset.UtcNow.ConvertToTimezone(timezone).DateOnly();
-                var endDate = currentDate.TranslateDateIfDaylightSavings(timezone, restrictionDays + 1);
-                if (endDate < end)
-                    end = endDate;
-                if (start > end)
-                    return Forbid();
+
+                if (!User.HasPermission(Permission.ViewAllFutureShifts))
+                {
+                    var shiftRestrictionDays = int.Parse(Configuration.GetNonEmptyValue("ViewShiftRestrictionDays"));
+                    var endDateShift = currentDate.TranslateDateIfDaylightSavings(timezone, shiftRestrictionDays + 1);
+                    foreach (var sa in shiftsWithDuties)
+                        sa.Conflicts = sa.Conflicts.WhereToList(c => c.Start < endDateShift);
+                }
+
+                if (!User.HasPermission(Permission.ViewDutyRosterInFuture))
+                {
+                    var dutyRestrictionHours =
+                        float.Parse(Configuration.GetNonEmptyValue("ViewDutyRosterRestrictionHours"));
+                    var endDateDuties =
+                        currentDate.TranslateDateForDaylightSavingsByHours(timezone, dutyRestrictionHours);
+                    foreach (var sa in shiftsWithDuties)
+                        sa.Conflicts.WhereToList(c => c.Start > endDateDuties)
+                            .ForEach(c => c.WorkSection = null);
+                }
             }
 
-            var shiftAvailability = await ShiftService.GetShiftAvailability(start, end, locationId: locationId);
-            var shiftsWithDuties = await DistributeScheduleService.GetDistributeSchedule(shiftAvailability, includeWorkSection, start, end, locationId);
             return Ok(shiftsWithDuties.Adapt<List<ShiftAvailabilityDto>>());
         }
     }
