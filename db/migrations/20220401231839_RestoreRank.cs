8178cdfb507db2cfb110d7b8d60485186aaab11b
nikola-rados <nrados@uvic.ca>
2022-04-01 17:02:22 -0700

    Migrations to restore rank

---
+++ db/migrations/20220401231839_RestoreRank.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SS.Db.Migrations
+{
+    public partial class RestoreRank : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rank",
+                table: "User",
+                type: "text",
+                nullable: true);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "User");
+        }
+    }
+}
---
+++ db/migrations/20220401232913_RankDataBackToUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SS.Db.Migrations
+{
+    public partial class RankDataBackToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"User\" SET \"Rank\" = \"SheriffRank\".\"Rank\" FROM \"SheriffRank\" WHERE \"SheriffRank\".\"SheriffId\" = \"User\".\"Id\"");
+
+            migrationBuilder.DropTable(name: "SheriffRank");
+        }
+    }
+}
